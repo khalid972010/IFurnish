47bd537626e8d61af26610396b9a02de76e536cd
Khalid Elsayed <khalid972010@gmail.com>
2024-04-29 22:47:19 +0300

    categories and seemore

--- i-furnish/src/pages/shop.jsx
+++ i-furnish/src/pages/shop.jsx
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ShopCard from "../components/shopCard";
+import SimpleBackdrop from "../components/spinner";
+import Category from "../components/category";
 
 const Shop = () => {
+  const products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [isShown, setShow] = useState(false);
+  function handleShowMore() {
+    setShow(!isShown);
+  }
   return (
-    <section className="brand_section layout_padding">
-      <div className="container">
-        <div className="heading_container">
-          <h2>Featured Brands</h2>
+    <>
+      <section className="brand_section layout_padding">
+        <div className="container">
+          <div className="heading_container">
+            <h2>Categories</h2>
+          </div>
+          <div className="categories ">
+            <Category type="Chair"></Category>
+            <Category type="Couch"></Category>
+            <Category type="Bed"></Category>
+            <Category type="Rug"></Category>
+          </div>
+          <div className="heading_container">
+            <h2>Featured Brands</h2>
+          </div>
+
+          <div
+            className="brand_container layout_padding1 "
+            style={{ width: "75vw" }}>
+            {isShown
+              ? products.map((e) => <ShopCard key={e} />)
+              : products.slice(0, 4).map((e) => <ShopCard key={e} />)}
+          </div>
+          <Link to="#" className="brand-btn" onClick={handleShowMore}>
+            {!isShown ? "See More" : "See Less"}
+          </Link>
         </div>
-        <div
-          className="brand_container layout_padding2 "
-          style={{ width: "75vw" }}
-        >
-          <ShopCard isNew={true}></ShopCard>
-          <ShopCard></ShopCard>
-          <ShopCard></ShopCard>
-          <ShopCard></ShopCard>
-        </div>
-        <Link to="#" className="brand-btn">
-          See More
-        </Link>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
